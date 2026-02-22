2258ffb3c188f143134e699d2a56e9cb0265b176
Jamal Salem <jaudi990@yahoo.com>
2026-02-22 09:06:50 +0400

    sketch.js

---
+++ mousePressed_keyPressed/sketch.js
@@ -0,0 +1,35 @@
+function setup()
+{
+	createCanvas(600, 600);
+	fill(255, 255, 255);
+
+}
+
+function draw()
+{
+	background(0, 0, 0);
+
+	ellipse(mouseX,mouseY, 100, 100);
+
+}
+
+
+function mousePressed()
+{
+	
+	fill(255,0,0);
+	
+	
+}
+
+
+function keyPressed()
+{
+	
+	
+	fill(0,255,255);
+	
+	
+	
+	
+}
