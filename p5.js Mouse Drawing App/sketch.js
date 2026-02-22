5af79592fc8509baae5318f0885c996057f6f4ed
Jamal Salem <jaudi990@yahoo.com>
2026-02-22 10:45:02 +0400

    sketch.js

---
+++ p5.js Mouse Drawing App/sketch.js
@@ -0,0 +1,15 @@
+function setup()
+{
+	createCanvas(800, 600);
+	background(255);
+}
+
+
+
+function draw()
+{
+    fill(0);             // لون أسود
+    noStroke();          // بدون حدود
+    ellipse(mouseX, mouseY, 10, 10);
+   
+}
